fa3b5490205badca32a441cfeb4935f32ac4e265
Ethan <ethanwright98@hotmail.co.uk>
2022-05-19 09:27:31 +0000

    adding api2

--- api1/app.py
+++ api1/app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route('/get/weapon', methods =['GET'])
 def get_weapon():
-    weapons = ['Claymore', 'Fists', 'Spear', 'Battle axe']
-    return Response(weapons[randint(0,3)], mimetype='text/plain')
+    weapons = ['Claymore', 'Fists', 'Spear', 'Battle axe', 'Bow']
+    return Response(weapons[randint(0,4)], mimetype='text/plain')
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True, host='0.0.0.0')
